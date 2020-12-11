caefd1d7992413620ec1ec77f5f5acfb7f0a9682
panq77 <365487740@qq.com>
2020-12-11 15:23:55 +0800

    Initial commit

---
+++ src/main/java/pq/empm/EmpmApplication.java
@@ -0,0 +1,13 @@
+package pq.empm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpmApplication.class, args);
+    }
+
+}
